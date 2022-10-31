86042303009d7797475936de532c9c7b2b0981b7
mimseyedi <mim.seyedi@gmail.com>
2022-10-31 16:34:42 +0330

    Initializing map script

---
+++ scripts/map.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import subprocess
+try:
+    from rich.console import Console
+except ImportError as module:
+    subprocess.run([sys.executable, "-m", "pip", "install", "rich"], stdout=subprocess.DEVNULL)
+
+
+def init():
+    screen = Console()
+
+    if len(sys.argv) == 1:
+        screen.print(os.getcwd())
+    elif len(sys.argv) == 2 and sys.argv[1] == "-h":
+        screen.print("With the map command you can see your current directory or location!", style="green")
+    else:
+        screen.print("Error: Unknown parameters!", style="red")
+
+
+if __name__ == "__main__":
+    init()
