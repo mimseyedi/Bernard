ac9f738dd6a1e65e476e937e4225f0ea138e6d4a
mimseyedi <mim.seyedi@gmail.com>
2022-10-31 20:14:04 +0330

    Initializing date script

---
+++ scripts/date.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import datetime
+import subprocess
+try:
+    from rich.console import Console
+except ImportError as module:
+    subprocess.run([sys.executable, "-m", "pip", "install", "rich"], stdout=subprocess.DEVNULL)
+finally:
+    screen = Console()
+
+
+guide_message = """With the date command you can see the date!
+
+Parameters:
+-j Jalali date"""
+
+
+def gregorian_to_jalali(gy, gm, gd):
+    g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+
+    if (gm > 2): gy2 = gy + 1
+    else: gy2 = gy
+
+    days = 355666 + (365 * gy) + ((gy2 + 3) // 4) - ((gy2 + 99) // 100) + ((gy2 + 399) // 400) + gd + g_d_m[gm - 1]
+    jy = -1595 + (33 * (days // 12053))
+    days %= 12053
+    jy += 4 * (days // 1461)
+    days %= 1461
+
+    if (days > 365):
+        jy += (days - 1) // 365
+        days = (days - 1) % 365
+
+    if (days < 186):
+        jm = 1 + (days // 31)
+        jd = 1 + (days % 31)
+    else:
+        jm = 7 + ((days - 186) // 30)
+        jd = 1 + ((days - 186) % 30)
+
+    return [jy, jm, jd]
+
+
+def init():
+    if len(sys.argv) == 1:
+        now = datetime.datetime.today()
+        screen.print(f"{now.day}/{now.month}/{now.year}")
+
+    elif len(sys.argv) == 2 and sys.argv[1] == "-j":
+        now = datetime.datetime.today()
+        jalali_date = gregorian_to_jalali(now.year, now.month, now.day)
+        screen.print(f"{jalali_date[0]}/{jalali_date[1]}/{jalali_date[2]}")
+
+    elif len(sys.argv) == 2 and sys.argv[1] == "-h":
+        screen.print(guide_message, style="green")
+
+    else:
+        screen.print("Error: Unknown parameters!", style="red")
+
+
+if __name__ == "__main__":
+    init()
