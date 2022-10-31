88c470d574bf661e72042d09a43479dd02bb5510
mimseyedi <mim.seyedi@gmail.com>
2022-10-31 15:08:35 +0330

    Initializing Bernard.py
    First of all Bernard's description added.
    Then packages and libraries added.
    Finally init method created to set start point.

---
+++ Bernard.py
@@ -0,0 +1,34 @@
+"""
+Bernard Version 1.0
+
+Bernard is an assistant and application to customize the terminal to do the tasks you need,
+which can be run on all bash Unix shells. Bernard consists of a script reader and a directory of scripts.
+With the help of script reader, Bernard can read and execute smaller programs by Python interpreter.
+In this way, it will be easier to develop Bernard by writing small and separate programs.
+
+Bernard is open source under the MIT license and you can easily use it
+and make any changes you like and share it with others.
+
+Bernard github repository:
+https://github.com/mimseyedi/Bernard
+"""
+
+
+import os
+import sys
+import json
+import sqlite3
+import subprocess
+from datetime import datetime
+from rich.console import Console
+from prompt_toolkit import PromptSession
+from prompt_toolkit.history import InMemoryHistory
+from prompt_toolkit.completion import WordCompleter
+
+
+def init():
+    pass
+
+
+if __name__ == "__main__":
+    init()
