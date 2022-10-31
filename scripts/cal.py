9eca1cb3de9a5389c691f95fc6160c87b226f024
mimseyedi <mim.seyedi@gmail.com>
2022-10-31 20:19:54 +0330

    Initializing cal script

---
+++ scripts/cal.py
@@ -0,0 +1,29 @@
+import sys
+import subprocess
+try:
+    from rich.console import Console
+except ImportError as module:
+    subprocess.run([sys.executable, "-m", "pip", "install", "rich"], stdout=subprocess.DEVNULL)
+finally:
+    screen = Console()
+
+
+guide_message = """With the cal command you can see the calendar!
+
+Parameters:
+-y Year view"""
+
+
+def init():
+    if len(sys.argv) == 1:
+        subprocess.run(["cal"])
+    elif len(sys.argv) == 2 and sys.argv[1] == "-y":
+        subprocess.run(["cal", "-y"])
+    elif len(sys.argv) == 2 and sys.argv[1] == "-h":
+        screen.print(guide_message, style="green")
+    else:
+        screen.print("Error: Unknown parameters!", style="red")
+
+
+if __name__ == "__main__":
+    init()
