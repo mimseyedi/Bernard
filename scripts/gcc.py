738e6f7d38f8efbc84c3d6d4f709147ca43f8d97
mimseyedi <mim.seyedi@gmail.com>
2022-10-31 18:48:58 +0330

    Initializing gcc script

---
+++ scripts/gcc.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import subprocess
+try:
+    from rich.console import Console
+except ImportError as module:
+    subprocess.run([sys.executable, "-m", "pip", "install", "rich"], stdout=subprocess.DEVNULL)
+finally:
+    screen = Console()
+
+
+def init():
+    if len(sys.argv) == 1:
+        subprocess.run(["gcc"])
+    elif len(sys.argv) == 2 and sys.argv[1] == "-h":
+        screen.print("With the gcc command, you can compile C codes.", style="green")
+    elif len(sys.argv) >= 2 and sys.argv[1] != "-h":
+        gcc_arg = " ".join(sys.argv[1:])
+        os.system(f"gcc {gcc_arg}")
+
+
+if __name__ == "__main__":
+    init()
