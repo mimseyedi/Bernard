280a365ce69cf6a63d7ae139fd6f5725cdd8564c
mimseyedi <mim.seyedi@gmail.com>
2022-11-08 19:09:41 +0330

    File existence checker added

--- scripts/read.py
+++ scripts/read.py
@@ -20,24 +20,27 @@ def init():
 
     elif len(sys.argv) == 2 and sys.argv[1] != "-h":
         file = f"{os.getcwd()}/{sys.argv[1]}" if sys.argv[1] in os.listdir() else sys.argv[1]
-        if os.path.isfile(file):
-            language = {".c": "c", ".h": "c", ".cpp": "c++", ".hpp": "c++",
-                        ".cs": "c#", ".css": "css", ".dart": "dart", ".go": "go",
-                        ".html": "html", ".htm": "html", ".xhtml": "html", ".java": "java",
-                        ".js": "js", ".jl": "julia", ".kt": "kotlin", ".m": "matlab",
-                        ".php": "php", ".py": "python", ".rs": "rust", ".scss": "scss",
-                        ".sql": "sql", ".swift": "swift", ".ts": "ts", ".vb": "vbnet",
-                        ".xml": "xml", ".txt": "txt"}
-            try:
-                with open(file, "r") as open_file:
-                    screen.print(Syntax(open_file.read(), language[f".{file.split('.')[-1]}"],
-                                        theme='monokai', line_numbers=True))
-            except UnicodeDecodeError:
-                screen.print(f"Error: Cant read this file! -> '{sys.argv[1]}'", style="red")
-            except KeyError:
-                screen.print(f"Error: Cant read this file! -> '{sys.argv[1]}'", style="red")
+        if os.path.exists(file):
+            if os.path.isfile(file):
+                language = {".c": "c", ".h": "c", ".cpp": "c++", ".hpp": "c++",
+                            ".cs": "c#", ".css": "css", ".dart": "dart", ".go": "go",
+                            ".html": "html", ".htm": "html", ".xhtml": "html", ".java": "java",
+                            ".js": "js", ".jl": "julia", ".kt": "kotlin", ".m": "matlab",
+                            ".php": "php", ".py": "python", ".rs": "rust", ".scss": "scss",
+                            ".sql": "sql", ".swift": "swift", ".ts": "ts", ".vb": "vbnet",
+                            ".xml": "xml", ".txt": "txt"}
+                try:
+                    with open(file, "r") as open_file:
+                        screen.print(Syntax(open_file.read(), language[f".{file.split('.')[-1]}"],
+                                            theme='monokai', line_numbers=True))
+                except UnicodeDecodeError:
+                    screen.print(f"Error: Cant read this file! -> '{sys.argv[1]}'", style="red")
+                except KeyError:
+                    screen.print(f"Error: Cant read this file! -> '{sys.argv[1]}'", style="red")
+            else:
+                screen.print("Error: You must select a file!", style="red")
         else:
-            screen.print("Error: You must select a file!", style="red")
+            screen.print("Error: File can not found!", style="red")
 
     else:
         screen.print("Error: Unknown parameters!", style="red")
