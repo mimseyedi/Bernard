fc29a22fd43817978a6253eb5a28927369b2146c
mimseyedi <mim.seyedi@gmail.com>
2022-11-14 17:09:29 +0330

    The paths were modified by the pathlib library
    This was done for compatibility with all operating systems

--- scripts/newf.py
+++ scripts/newf.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+from pathlib import Path
 try:
     from rich.console import Console
 except ImportError as module:
@@ -18,11 +19,12 @@ def init():
         screen.print("With the newf command, you can create a file.", style="green")
 
     elif len(sys.argv) == 2 and sys.argv[1] != "-h":
-        if os.path.exists(f'{os.getcwd()}/{sys.argv[1]}'):
+        file_path = Path(os.getcwd(), sys.argv[1])
+        if file_path.exists():
             screen.print("Error: There is a file with this name!", style="red")
         else:
-            with open(f"{os.getcwd()}/{sys.argv[1]}", "w") as _: pass
-            screen.print(f"'{sys.argv[1]}' file successfully created.", style='green')
+            with open(file_path, "w") as _: pass
+            screen.print(f"'{file_path.name}' file successfully created.", style='green')
 
     else:
         screen.print("Error: Unknown parameters!", style="red")
