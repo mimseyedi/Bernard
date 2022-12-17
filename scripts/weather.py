2ffef86e28431e498844efe173293b4174b1e4b4
mimseyedi <mim.seyedi@gmail.com>
2022-12-18 02:29:38 +0330

    The display of the weather condition was modified and the parameters were modified according to the development standards of Bernard. Jalali date was also added

--- scripts/weather.py
+++ scripts/weather.py
@@ -1,29 +1,14 @@
-#importing modules
+import os
 import sys
+import json
+import datetime
 import subprocess
-
-try:
-     import calendar
-except ImportError as module:
-    subprocess.run([sys.executable, "-m", "pip", "install", "requests"], stdout=subprocess.DEVNULL)
-finally:
-     import calendar
-
 try:
      import requests
 except ImportError as module:
     subprocess.run([sys.executable, "-m", "pip", "install", "requests"], stdout=subprocess.DEVNULL)
 finally:
      import requests
-
-
-try:
-     import json
-except ImportError as module:
-    subprocess.run([sys.executable, "-m", "pip", "install", "requests"], stdout=subprocess.DEVNULL)
-finally:
-     import json
-
 try:
     from rich.console import Console
 except ImportError as module:
@@ -33,58 +18,102 @@ finally:
     screen = Console()
 
 
-# Start-point.
-guide_message = "By using the weather command, you can watch the latest news of the weather in every city you want."
+# A message for guidance and how to use the script.
+guide_message = """By using the weather command, you can watch the latest news of the weather in every city you want.
+
+Parameters:
+weather <city> -> example: weather Tehran
+-w weekly weather -> example: weather -w New York
+"""
+
+
+# Global variables:
+API_URL = "https://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "b07a711bdf19e559aca44794573fe62e"
-forcast_url = 'https://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY
+FORCAST_URL = 'https://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY
 
-def init():
- try:
-    # If the script is called alone.
-    # Default weather.
-    if len(sys.argv) == 1:
-        # Asks the user for the city or zip code to be queried
-        forcast_url = 'https://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY
-        while True:
 
-                    city = input('Please input the city name: ')
-                    # Appends the city to the api call
-                    forcast_url += '&q=' + city
-                    break
+# The function to convert the Gregorian date to Jalali is taken from the following link:
+# https://jdf.scr.ir/jdf/python
+def gregorian_to_jalali(gy, gm, gd):
+    g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
 
-        # Stores the Json response
-        json_data = requests.get(forcast_url).json()
+    if (gm > 2): gy2 = gy + 1
+    else: gy2 = gy
 
-        location_data = {
-            'city': json_data['city']['name'],
-            'country': json_data['city']['country']
-        }
+    days = 355666 + (365 * gy) + ((gy2 + 3) // 4) - ((gy2 + 99) // 100) + ((gy2 + 399) // 400) + gd + g_d_m[gm - 1]
+    jy = -1595 + (33 * (days // 12053))
+    days %= 12053
+    jy += 4 * (days // 1461)
+    days %= 1461
 
-        screen.print('\n{city}, {country}'.format(**location_data), style="blue")
+    if (days > 365):
+        jy += (days - 1) // 365
+        days = (days - 1) % 365
 
-        # The current date we are iterating through
-        current_date = ''
+    if (days < 186):
+        jm = 1 + (days // 31)
+        jd = 1 + (days % 31)
+    else:
+        jm = 7 + ((days - 186) // 30)
+        jd = 1 + ((days - 186) % 30)
 
-        # Iterates through the array of dictionaries named list in json_data
-        for item in json_data['list']:
+    return [jy, jm, jd]
 
-            # Time of the weather data received, partitioned into 3 hour blocks
+
+def get_todays_weather(city: str):
+    API_URL_MATCH = API_URL + "q=" + city + "&appid=" + API_KEY
+
+    request = requests.get(API_URL_MATCH)
+    data = request.json()
+
+    if data["cod"] == 200:
+        main_data = data["main"]
+
+        screen.print(f"Weather condition of '{city}' city:", style="yellow")
+
+        now = datetime.datetime.today()
+        jalali_y, jalali_m, jalali_d = gregorian_to_jalali(now.year, now.month, now.day)
+
+        screen.print(f"   └── G: {now.month}/{now.day}/{now.year} - J: {jalali_y}/{jalali_m}/{jalali_d}", style="yellow")
+        screen.print(f"       ├── Temperature: {main_data['temp'] - 273.15}", style="yellow")
+        screen.print(f"       ├── Feels Like: {main_data['feels_like'] - 273.15}", style="yellow")
+        screen.print(f"       ├── Humidity: {main_data['humidity']}", style="yellow")
+        screen.print(f"       └── Pressure: {main_data['pressure']}", style="yellow")
+
+    elif data["cod"] == '404':
+        screen.print(f"Error: '{city.capitalize()}' city not found!", style="red")
+
+    elif data["cod"] == '503':
+        screen.print("Error: No internet connection!", style="red")
+
+    else:
+        screen.print("Error: The API is currently unavailable!", style="red")
+
+
+def get_weekly_weather(city: str):
+    FORCAST_URL_MATCH = FORCAST_URL + '&q=' + city
+
+    request = requests.get(FORCAST_URL_MATCH)
+    data = request.json()
+
+    if data["cod"] == '200':
+        screen.print(f"Weather condition of {city} this week:")
+
+        current_date = str()
+
+        for item in data['list']:
             time = item['dt_txt']
-
-            # Split the time into date and hour [2018-04-15 06:00:00]
             next_date, hour = time.split(' ')
 
-            # Stores the current date and prints it once
             if current_date != next_date:
                 current_date = next_date
                 year, month, day = current_date.split('-')
-                date = {'y': year, 'm': month, 'd': day}
-                screen.print('\n{m}/{d}/{y}'.format(**date), style="blue")
+                jalali_y, jalali_m, jalali_d = gregorian_to_jalali(int(year), int(month), int(day))
+                screen.print(f'\nG: {month}/{day}/{year} - J: {jalali_y}/{jalali_m}/{jalali_d}', style="yellow")
 
-            # Grabs the first 2 integers from our HH:MM:SS string to get the hours
             hour = int(hour[:2])
 
-            # Sets the AM (ante meridiem) or PM (post meridiem) period
             if hour < 12:
                 if hour == 0:
                     hour = 12
@@ -94,26 +123,49 @@ def init():
                     hour -= 12
                 meridiem = 'PM'
 
-            # Prints the hours [HH:MM AM/PM]
-            screen.print('\n%i:00 %s' % (hour, meridiem))
+            screen.print(f'   ├── {hour}:00 {meridiem}', style="yellow")
 
-            # Temperature is measured in Kelvin
             temperature = item['main']['temp']
-
-            # Weather condition
             description = item['weather'][0]['description'],
 
-            # Prints the description as well as the temperature in Celcius and Farenheit
-            screen.print('Weather condition: %s' % description, style="blue")
-            screen.print('Celcius: {:.2f}'.format(temperature - 273.15), style="blue")
+            screen.print(f'   │    ├── Weather condition: {description[0]}', style="yellow")
+            screen.print( '   │    └── Celcius: {:.2f}°'.format(temperature - 273.15), style="yellow")
+
+    elif data["cod"] == '404':
+        screen.print(f"Error: '{city.capitalize()}' city not found!", style="red")
+
+    elif data["cod"] == '503':
+        screen.print("Error: No internet connection!", style="red")
+
+    else:
+        screen.print("Error: The API is currently unavailable!", style="red")
 
 
-     # Display help and description of the called script with -h parameter.
+# Start-point.
+def init():
+    # If the script is called alone.
+    if len(sys.argv) == 1:
+        screen.print("Error: You must select a city!", style="red")
+
+    # If the script is called with the -h parameter.
+    # Display help and description of the called script with -h parameter.
     elif len(sys.argv) == 2 and sys.argv[1] == "-h":
-     screen.print(guide_message, style="green")
+        screen.print(guide_message, style="green")
+
+    # If the script is called with any parameter except -h and -w.
+    # Pattern: weather Tehran
+    #          weather New York
+    elif len(sys.argv) <= 3 and sys.argv[1] not in  ["-h", "-w"]:
+        get_todays_weather(city=' '.join(sys.argv[1:]))
+
+    # If the script is called with the -w parameter.
+    # Pattern: weather -w Tehran
+    #          weather -w Los Angeles
+    elif len(sys.argv) >= 3 and sys.argv[1] == "-w":
+        get_weekly_weather(city=' '.join(sys.argv[2:]))
 
     # If none of these.
- except:
+    else:
         screen.print("Error: Unknown parameters!", style="red")
 
 
